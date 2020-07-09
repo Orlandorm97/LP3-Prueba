b94020d9aab02daadf8aaede93681dadcf9ff758
Orlando <orlando97rm@gmail.com>
2020-07-09 16:17:46 -0500

    Autor. LISTO

--- aprobado.py
+++ aprobado.py
@@ -1,17 +1,17 @@
-
+# Autor: Orlando Ramos Machuca
 
 def determinarAprobado(promedio):
     
-    if 0<=promedio<=10:
+    if 0<=promedio<=10.4:
         print("El alumno está desaprobado.")
         
-    elif 20>=promedio>10:
+    elif 20>=promedio>=10.5:
         print("El alumno está aprobado.")
     
     else:
         print("La entrada es incorrecta.")
         
         
-promedio = int(input("Ingrese el promedio: "))
+promedio = float(input("Ingrese el promedio: "))
 
 determinarAprobado(promedio)
